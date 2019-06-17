b0d17c65c9bf60823ac4970e773f0b60a7646a96
lantian <1062217098@qq.com>
2019-06-17 23:57:06 +0800

    以调用者角度理解metaclass和orm

--- temp/web_orm.py
+++ temp/web_orm.py
@@ -237,3 +237,90 @@ class TextField(Field):
 class BooleanField(Field):
     def __init__(self, name=None, default=False):
         super().__init__(name, 'bool', None, default)
+
+# metaclass定义元类
+class ModelMetaclass(type):
+
+    def __new__(cls, name, bases, attrs):
+
+        if name == 'Model': # 这里跳过对Model这个类的修改，当类为Model时，直接由type创建
+            return type.__new__(cls, name, bases, attrs)
+
+        tableName = attrs.get('__table__', None) or name # 获取表名，如果没有__table__字段，则定义为class的name
+        logging.info('found model: %s (table: %s)' % (name, tableName))
+
+        mappings = dict()
+        fields = []
+        primaryKey = None:
+        for k, v in attrs.items():
+            if isinstance(v, Field):
+                # 已存在的映射关系
+                logging.info('  found mapping: %s ==> %s' % (k, v))
+                # 所有的映射关系都存储在mappings列表中
+                mappings[k] = v
+                if v.primary_key:
+                    if primaryKey:
+                        raise RuntimeError('Duplicate primary key for field: %s' % k)
+                    primaryKey = k
+                else:
+                    # 将映射关系分为了主键primary_key和fields
+                    fields.append(k)
+
+        if not primaryKey:
+            raise RuntimeError('Primary key not found.')
+        for k in mappings.keys():
+            # 从类属性中删除mappings中存在的映射属性，避免出现运行时错误
+            attrs.pop(k)
+        escaped_fields = list(map(lambda f: '`%s`' % f, fields))
+        attrs['__mappings__'] = mappings
+        attrs['__table__'] = tableName
+        attrs['__primary_key__'] = primaryKey
+        attrs['__fields__'] = fields
+        attrs['__select__'] = 'select `%s`, %s from `%s`' % (primaryKey, ','.join(escaped_fields), tableName)
+        attrs['__insert__'] = 'insert into `%s` (%s, `%s`) values (%s)' % (tableName, ','.join(escaped_fields), primaryKey, create_args_string(len(escaped_fields) + 1))
+        attrs['__update__'] = 'update `%s` set %s where `%s`=?' % (tableName, ','.join(map(lambda f: '`s`=?' % (mappings.get(f).name or f), fields)), primaryKey)
+        attrs['__delete__'] = 'delete from `%s` where `%s`=?' % (tableName, primaryKey)
+        return type.__new__(cls, name, bases, attrs)
+
+class Model(dict, metaclass=ModelMetaclass):
+
+    def __init__(self, **kw):
+        super(Model, self).__init__(**kw)
+
+    def __getattr__(self, key):
+        try:
+            return self[key]
+        except KeyError:
+            raise AttributeError(r"'Model' object has no attribute '%s'" % key)
+
+    def __setattr__(self, key, value):
+        self[key] = value
+
+    def getValue(self, key):
+        return getattr(self, key, None)
+
+    def getValueOfDefault(self, key):
+        value = getattr(self, key, None)
+        if value is None:
+            field = self.__mappings__[key]
+            if field.default is not None:
+                value = field.default() if callable(field.default) else field.default
+                logging.debug('usign defalut value for %s: %s' 5 (key, str(value)))
+                setattr(self, key, value)
+        return value
+
+    @classmethod
+    async def find(cls, pk):
+        '''find object by primary key.'''
+        rs = await select('%s where `%s`=?' % (cls.__select__, cls.__primary_key__), [pk], 1)
+        if len(rs) == 0:
+            return None
+        # conn.cursor(aiomysql.DictCursor)返回结果为字典
+        return cls(**rs[0])
+
+    async def save(self):
+        args = list(map(self.getValueOfDefault, self.__fields__))
+        args.append(self.getValueOfDefault(self.__primary_key__))
+        rows = await execute(self.__insert__, args)
+        if rows != 1:
+            logging.warn('failed to insert record: affected rows: %s' % rows)
