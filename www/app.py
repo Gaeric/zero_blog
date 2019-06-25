444ce31ea4d0bd3f0e69d38975c0a5dbd5d5df0d
lantian <1062217098@qq.com>
2019-06-25 21:59:16 +0800

    setp 1 build a web server

--- www/app.py
+++ www/app.py
@@ -3,7 +3,6 @@
 
 __author__ = 'zero'
 
-
 '''
 A async web application
 '''
@@ -16,4 +15,19 @@ import time
 from datetime import datetime
 from aiohttp import web
 
-from web_frame import add_static, add_routes
+logging.basicConfig(level=logging.DEBUG)
+
+# 首先建立一个web server服务器
+async def init():
+    "Build web app"
+    app = web.Application()
+
+    runner = web.AppRunner(app)
+    await runner.setup()
+    site = web.TCPSite(runner, '127.0.0.1', 9000)
+    logging.info('server started at http: //127.0.0.1:9000...')
+    await site.start()
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(init())
+loop.run_forever()
