28e72bc66f38eb69b21d1d6ff394455b1f5ea3fb
lantian <1062217098@qq.com>
2019-06-25 22:53:41 +0800

    Setp3: Add template

--- www/app.py
+++ www/app.py
@@ -16,8 +16,31 @@ from datetime import datetime
 from aiohttp import web
 
 from web_frame import add_routes
+from jinja2 import Environment
+from jinja2 import FileSystemLoader
 
 logging.basicConfig(level=logging.DEBUG)
+
+def init_jinja2(app, **kw):
+    logging.info('init jinja2...')
+    options = dict(
+        autoescape = kw.get('autoescape', True),
+        block_start_string = kw.get('block_start_string', '{%'),
+        block_end_string = kw.get('block_end_string', '%}'),
+        variable_start_string = kw.get('variable_start_string', '{{'),
+        variable_end_string = kw.get('variable_end_string', '}}'),
+        auto_reload = kw.get('auto_reload', True)
+    )
+    path = kw.get('path', None)
+    if path is None:
+        path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
+    logging.info(f'set jinja2 template path: {path}')
+    env = Environment(loader=FileSystemLoader(path), **options)
+    filters = kw.get('filters', None)
+    if filters is not None:
+        for name, f in filters.items():
+            env.filters[name] = f
+    app['__templating__'] = env
 
 async def logger_factory(app, handler):
     async def logger(request):
@@ -55,18 +78,16 @@ async def response_factory(app, handler):
             return resp
         if isinstance(r, dict):
             template = r.get('__template__')
+            logging.info(f'template is {template}')
             if template is None:
                 resp = web.Response(body=json.dumps(r, ensure_ascii=False, defalut=lambda o: o.__dict__).encode('utf-8'))
                 resp.content_type = 'application/json;charset=utf-8'
                 return resp
-        else:
-            # app['__templateing__'] 是用于jinja2模板的方法
-            # resp = web.Response(body=app['__templating__'].get_template(template).render(**r).encode('utf-8'))
-            template = r
-            logging.info(f'Template of Response is: {template}')
-            resp = web.Response(body=template.encode('utf-8'))
-            resp.content_type = 'text/html;charset=utf-8'
-            return resp
+            else:
+                # app['__templateing__'] 是用于jinja2模板的方法
+                resp = web.Response(body=app['__templating__'].get_template(template).render(**r).encode('utf-8'))
+                resp.content_type = 'text/html;charset=utf-8'
+                return resp
         if isinstance(r, int) and r >= 100 and r < 600:
             return web.Response(r)
         if isinstance(r, tuple) and len(r) == 2:
@@ -76,7 +97,6 @@ async def response_factory(app, handler):
         # default
         resp = web.Response(body=str(r).encode('utf-8'))
         resp.content_type = 'text/plain;charset=utf-8'
-        logging.inof("Response handler end.")
         return resp
     return response
 
@@ -90,6 +110,8 @@ async def init():
         logger_factory, response_factory
     ])
 
+    # 添加jinja2模板
+    init_jinja2(app)
     # 添加路由
     add_routes(app, 'handlers')
 
